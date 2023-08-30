104f2737f30032f9f989d4b603f3cf7f5e9daebc
tarasowski <dimitri+github@tarasowski.de>
2023-08-30 09:27:52 +0200

    feat: added object aufgabe

---
+++ object.js
@@ -0,0 +1,25 @@
+/* 
+Aufgabe: Objekt-Abenteuer
+
+Deine Aufgabe besteht darin, ein einfaches JavaScript-Programm zu 
+erstellen, das ein Abenteuer in einem Objekt darstellt. 
+Du hast ein Objekt namens abenteuer, das Informationen über verschiedene 
+Stationen eines Abenteuers enthält. 
+Jede Station wird durch einen Namen und eine Beschreibung repräsentiert.
+
+*/
+
+const abenteuer = {
+    start: "Du stehst am Anfang deines Abenteuers.",
+    wald: "Du betrittst einen dunklen Wald voller Geheimnisse.",
+    fluss: "Du kommst an einen reißenden Fluss, der deinen Weg blockiert.",
+    hoehle: "In der Höhle leuchten seltsame Kristalle in allen Farben.",
+    schatz: "Du findest einen versteckten Schatz und spürst, dass dein Abenteuer erfolgreich war!",
+    berge: "Du befindest dich in den Bergen und suchst nach einem Schatz"
+    //  done Füge hier weitere Stationen hinzu, wenn du möchtest.
+  };
+  
+// TODO: Schreibe den Code hier, um das Abenteuer zu durchlaufen und die Beschreibungen auszugeben.
+
+
+  

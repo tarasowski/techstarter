ff264a0f5638d5e8ce3bd547d642216dc5df4014
tarasowski <dimitri+github@tarasowski.de>
2023-09-01 12:05:16 +0200

    feat: added classes cart

--- 01-09-2023/classes.js
+++ 01-09-2023/classes.js
@@ -3,33 +3,102 @@
 // kunde kann produkte hinzufügen, entfernen etc.
 // kunde kann bezahlen
 
+// wir sind JEFF BEZOS
+
 class Product {
-    // price
-    // name
-    // id
+    constructor(price, name, id, category) {
+        this.price = price
+        this.name = name
+        this.id = id
+        this.category = category
+    }
 }
 
-//let p1 = ...
-//let p2 = ...
-//let p3 = ...
+let p1 = new Product(1000, "Fernseher", 1, "Entertainment") 
+
+/* 
+let p1 = {
+    price: 1000,
+    name: "Fernseher",
+    id: 1,
+    category: "Entertainment"
+} 
+*/
+
+let p2 = new Product(2000, "Macbook Pro", 2, "Computer")
+
+let p3 = new Product(500, "Waschmaschine", 3, "Haushalt")
+
+console.log(
+   // p1,
+   // p2,
+   // p3
+)
 
 class ShoppingCart {
-    // products []
-    // total_price 
-    // addProduct
-    // removeProduct
-    // listProducts
+    constructor() {
+        this.products = []
+        this.totalPrice = 0
+    }
+    // ⬇️ are methods
+    addingToCart(product) {
+        console.log("Das Produkt wird hinzugefügt: ", product)
+        this.products.push(product)
+        this.totalPrice += product.price
+    }
+    deleteFromCart(product) {
+        console.log("Das Produkt wird entfernt: ", product)
+        this.totalPrice -= product.price
+        //this.products = this.products.filter(p => p.id !== product.id)
+        let idx = this.products.findIndex(p => p.id === product.id)
+        // === equal
+        // !== not equal
+        if (idx !== -1) {
+            this.products.splice(idx, 1)
+        }
+
+    }
+    printCart() {
+        console.log(
+            "Here sind alle Produkte aus dem Warenkorb: 💰",
+             this.products,
+             "Das ist der Endpreis: 💵",
+             this.totalPrice
+            )
+    }
+    listProducts(){
+                                            // string literals
+        return this.products.map(product => `Produktname: ${product.name} - Produktpreis: ${product.price}`)
+    }
     // replaceProduct
-    // showFinalPrice
     // payFinalPrice
 }
 
-// let cart = ...
-// cart.addProduct
-// cart.addProduct
-// cart.addProduct
-// cart.listProducts
-// cart.removeProduct
-// cart.replaceProduct
-// cart.showFinalPrice
-// cart.payFinalPrice
+let cart = new ShoppingCart()
+
+cart.addingToCart(p1)
+cart.addingToCart(p2)
+cart.addingToCart(p3)
+cart.deleteFromCart(p1)
+
+console.log(
+    cart.listProducts()
+)
+
+// dot notation
+//console.log("Preis", cart.totalPrice)
+//console.log("Products", cart.products)
+
+// bracket(s) [] notation
+console.log("Preis", cart["totalPrice"] )
+console.log("Products", cart["products"])
+
+
+
+// bracket notation
+//cart.printCart()
+
+
+//console.log(cart)
+
+//const xs = [1, 2, 3, 4, 5]

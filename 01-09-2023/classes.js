3fb184196a65f89f0842bf3bbda4460c00b0d2ed
tarasowski <dimitri+github@tarasowski.de>
2023-09-01 08:24:29 +0200

    feat: added classes exercise

---
+++ 01-09-2023/classes.js
@@ -0,0 +1,35 @@
+// wir bauen ein online shop
+// betreiber kann produkte erstellen
+// kunde kann produkte hinzufügen, entfernen etc.
+// kunde kann bezahlen
+
+class Product {
+    // price
+    // name
+    // id
+}
+
+//let p1 = ...
+//let p2 = ...
+//let p3 = ...
+
+class ShoppingCart {
+    // products []
+    // total_price 
+    // addProduct
+    // removeProduct
+    // listProducts
+    // replaceProduct
+    // showFinalPrice
+    // payFinalPrice
+}
+
+// let cart = ...
+// cart.addProduct
+// cart.addProduct
+// cart.addProduct
+// cart.listProducts
+// cart.removeProduct
+// cart.replaceProduct
+// cart.showFinalPrice
+// cart.payFinalPrice

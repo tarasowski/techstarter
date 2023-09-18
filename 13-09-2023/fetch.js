1bdb2147616a2dac9caa5d3ba63c20946f1f4ea2
tarasowski <dimitri+github@tarasowski.de>
2023-09-18 13:08:00 +0200

    feat: added nodejs server

--- 13-09-2023/fetch.js
+++ 13-09-2023/fetch.js
@@ -1,8 +1,7 @@
 const url = "https://api.brightsky.dev/weather?lat=52.52&lon=13.404954&date=2023-09-13"
 
-
 const getWeather = () => {
-   fetch(url).then(response => response.json())
+   return fetch(url).then(response => response.json())
              .then(console.log)
              .catch(console.error)
 }
@@ -10,7 +9,6 @@ const getWeather = () => {
 const getWeather2 = async () => {
     const response = await fetch(url) 
     const body = await response.json()
-    console.log(body)
     return body
 }
 
---
+++ 18-09-2023/server.js
@@ -0,0 +1,68 @@
+const express = require("express")
+// express is a constructor
+// a constructor creates an object
+// an object hast properties (keys/value) and methods (functions)
+const app = express()
+
+const port = 3000
+
+let count = 5
+
+const callback = (req, res) => {
+    count += 1
+    res.json( {message: "hello world", username: "Dimi"} )
+    /*
+    res.send(`
+        <h1>Soll neu dargestellt werden</h1>
+        <p>Das ist mein Text welher dargestellt werden soll</p>
+        <span>${count}</span>
+    `)
+    */
+}
+// app.get("/", callback(req, res))
+app.get("/", callback)
+
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id 
+        this.name = name
+        this.price = price
+        this.category = category
+    }
+}
+
+let products = []
+
+products.push(new Product(1, "LG TV", 500, "Entertainment"))
+products.push(new Product(2, "Panasonic TV", 900, "Entertainment"))
+products.push(new Product(3, "iPhone", 1500, "Productivity"))
+products.push(new Product(4, "iPhon Plus+++", 5000, "Productivity"))
+
+// /products -> is the route 
+app.get("/products", function(req, res) {
+    // read the products from db
+    // return the products
+    res.json(products)
+})
+
+let id = 4
+
+app.get("/products/add", function(req, res){
+    id += 1
+    products.push(new Product(id, "Test Product", 500, "Test Category"))
+    res.send("Product added")
+})
+
+// "/products/:id" -> **route** is the name for that
+app.get("/products/:myId/:myName", function(req, res) {
+    const myId = req.params.myId
+    const myName = req.params.myName
+
+    const myProduct = products.find(product => product.id === parseInt(myId))
+    res.json(myProduct)
+})
+
+// function() {} here is a callback
+app.listen(port, function() {
+    console.log(`Server läuft auf Port ${port}`)
+})

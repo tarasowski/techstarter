c08f3fc738a1d6762c8078bb747af52822864a31
tarasowski <dimitri+github@tarasowski.de>
2023-10-31 12:29:04 +0100

    feat: added lambda function

---
+++ 30-10-2023/lambda.py
@@ -0,0 +1,18 @@
+
+                    #👇 this function returns the same what it gets
+simpple_function =  lambda x: x + "!!!"  
+
+# x => x
+# () => {
+#    hello my next line
+#    hello my next lien
+# }
+
+def fn(x):
+    if True:
+        return x
+    else:
+        return "kein wert"
+
+
+
